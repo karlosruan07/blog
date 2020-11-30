6c1d790c55eb771a27d42cd527919596f7b3bc50
Ruan <karlosruan9@gmail.com>
2020-11-30 19:01:51 -0300

    5ª committ Ampliando a aplicação

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ from . import views #aqui importa todas as views do arquivo blog/views.py
 ]"""
 urlpatterns = [
     path('', views.post_list, name='post_list'), #quando for digitado na url um espaço em branco "sem subpastas" então irá cair nessa view
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #url criada, o post serve para dizer que a url tem que começar com o post, e o <int:pk>/ espera um objeto inteiro para tranferir para a view, esse nº inteiro é relacionado a chave primaria
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 #a biblioteca de 404 serve para retorna uma pagina de erro caso não exista uma determinada página
 #from .models import Post
 from blog.models import Post
 from django.utils import timezone
@@ -12,4 +12,8 @@ def post_list(request):
     #teste = Post.objects.all()  #Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     posts = Post.objects.order_by('-created_data')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
