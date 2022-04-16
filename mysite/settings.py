d8eb1a152a29504821341750ec2e756b7f9f06da
Ruan <karlosruan9@gmail.com>
2022-04-16 20:30:29 -0300

    habilitando debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#7rut#zz*24ici%0$wlww41*61dw0r!=oozx@c22oge4gpl#@='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -2,15 +2,15 @@
 from django.contrib import admin
 from django.urls import path, include #o include serve para incluir um arquivo interno da mesma pasta ou um arquivo de outra subpasta
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),#tudo o que virá sem diretórios na url será redireciondo para esse arquivo
     path('accounts/', include('accounts.urls')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+ 
